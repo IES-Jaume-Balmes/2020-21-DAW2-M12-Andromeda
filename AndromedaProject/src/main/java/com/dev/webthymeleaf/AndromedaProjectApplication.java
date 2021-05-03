1eb3e059ad7bde107e792738388da41f69769ae1
Dani <dgarcia19@jaumebalmes.net>
2021-05-04 01:18:36 +0200

    createProject

---
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/AndromedaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dev.webthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndromedaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndromedaProjectApplication.class, args);
+	}
+
+}
---
+++ AndromedaProject/src/test/java/com/dev/webthymeleaf/AndromedaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.webthymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndromedaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
