ffbe08274cf853306a133dfaaead4363595070e1
aaron <aayllanes19@jaumebalmes.net>
2021-05-05 20:49:32 +0200

    RepositoryUsuari nuevo

--- AndromedaProject/src/main/java/com/dev/webthymeleaf/controladores/ControladorRegistro.java
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/controladores/ControladorRegistro.java
@@ -2,25 +2,39 @@ package com.dev.webthymeleaf.controladores;
 
 import java.lang.ProcessBuilder.Redirect;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.dev.webthymeleaf.dao.RepositorioUsuario;
 import com.dev.webthymeleaf.entidades.Usuario;
+
+
 
 @Controller
 public class ControladorRegistro {
 	
+	@Autowired
+	RepositorioUsuario UsuarioRep;
+	
 	@GetMapping("/registro")
 	public String registrarNuevoUsuario(Model model) {
 		Usuario nuevo = new Usuario();
-		//model.addAttribute("registroUsuario", nuevo);
+		model.addAttribute("registroUsuario", nuevo);
 		
 		return "/register.html";
 		
 	}
 		
+	@PostMapping("/guardar")
+	public String guardado(Usuario nuevo,Model model) {
 		
+		UsuarioRep.save(nuevo);
+		
+		return "redirect:/";
+	}
 	
 	
 	
--- AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioUsuario.java
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioUsuario.java
@@ -2,12 +2,13 @@ package com.dev.webthymeleaf.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
 import com.dev.webthymeleaf.entidades.Usuario;
 
 
 
-public interface RepositorioUsuario extends CrudRepository<Usuario,Long>{
+public interface RepositorioUsuario extends JpaRepository<Usuario,Long>{
 
 }
