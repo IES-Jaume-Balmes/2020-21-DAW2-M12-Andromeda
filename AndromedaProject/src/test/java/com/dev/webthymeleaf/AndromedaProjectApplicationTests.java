23774c6c472d98e09e719b8e1ee819baff607955
Dani <dgarcia19@jaumebalmes.net>
2021-05-17 20:44:08 +0200

    Arreglo error

--- AndromedaProject/src/test/java/com/dev/webthymeleaf/AndromedaProjectApplicationTests.java
+++ AndromedaProject/src/test/java/com/dev/webthymeleaf/AndromedaProjectApplicationTests.java
@@ -1,6 +1,5 @@
 package com.dev.webthymeleaf;
 
-import org.h2.server.web.WebServlet;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
