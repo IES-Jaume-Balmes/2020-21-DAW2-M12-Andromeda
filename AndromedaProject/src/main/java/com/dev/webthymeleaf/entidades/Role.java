f06099754f6b000e9958e3969fcaf94b9ee4fa4a
AdrianaY <adyllanes19@jaumebalmes.net>
2021-05-17 20:13:25 +0200

    Funciona el registro de usuarios y se comprueba el login en la base de datos.
    Eliminados algunos archivos de configuracion (Spring security).

--- AndromedaProject/src/main/java/com/dev/webthymeleaf/entidades/Role.java
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/entidades/Role.java
@@ -1,16 +1,49 @@
 package com.dev.webthymeleaf.entidades;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
+
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "role")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	
+	public Role() {
+		
+	}
+	
+	public Role(String name) {
+		super();
+		this.name = name;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
+
+
+
+
+
+/*@Entity
 public class Roles {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -71,4 +104,4 @@ public class Roles {
 	}
 	
 	
-}
+}*/
