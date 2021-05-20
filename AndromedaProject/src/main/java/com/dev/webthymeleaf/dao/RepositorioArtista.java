265f104eb666057747fcfcbe2e728e226ee8ca58
Dani <dgarcia19@jaumebalmes.net>
2021-05-20 16:30:58 +0200

    pequeños cambios y query de repositorios corregida.

--- AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioArtista.java
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioArtista.java
@@ -3,6 +3,7 @@ package com.dev.webthymeleaf.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dev.webthymeleaf.entidades.Artista;
@@ -10,5 +11,6 @@ import com.dev.webthymeleaf.entidades.Artista;
 
 @Repository
 public interface RepositorioArtista extends JpaRepository<Artista,Long>{
-
+	@Query(value = "SELECT u.artista,u.id_artista, r.genero , r.cancion FROM cancion r INNER JOIN artista u ON u.id_artista= r.id_cancion", nativeQuery = true)
+    public List<Artista> findByArtista();
 }
--- AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioCancion.java
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioCancion.java
@@ -3,6 +3,7 @@ package com.dev.webthymeleaf.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.dev.webthymeleaf.entidades.Cancion;
@@ -10,5 +11,6 @@ import com.dev.webthymeleaf.entidades.Cancion;
 
 @Repository
 public interface RepositorioCancion extends JpaRepository<Cancion,Long>{
-
+	@Query(value = "SELECT u.artista,u.id_artista, r.genero , r.cancion FROM cancion r INNER JOIN artista u ON u.id_artista= r.id_cancion", nativeQuery = true)
+    public List<Cancion> findAll();
 }
