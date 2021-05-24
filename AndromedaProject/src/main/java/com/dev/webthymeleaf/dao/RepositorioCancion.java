ebeb059d88e96b34ced657238170aa56410efd23
aaron <aayllanes19@jaumebalmes.net>
2021-05-24 19:11:59 +0200

    Query de la api ahora se muestra la información apropiadamente

--- AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioCancion.java
+++ AndromedaProject/src/main/java/com/dev/webthymeleaf/dao/RepositorioCancion.java
@@ -12,6 +12,6 @@ import com.dev.webthymeleaf.entidades.Cancion;
 
 @Repository
 public interface RepositorioCancion extends JpaRepository<Cancion,Long>{
-	@Query(value = "SELECT u.artista,u.id_artista, r.genero , r.cancion FROM cancion r INNER JOIN artista u ON u.id_artista= r.id_cancion", nativeQuery = true)
+	@Query(value = "select cancion, artista from cancion c, artista a, cancion_artista ca where c.id_cancion=ca.id_cancion and ca.id_artista=a.id_artista order by c.cancion;", nativeQuery = true)
     public List<CancionArtistaDto> findByCancionArtistaDtos();
 }
