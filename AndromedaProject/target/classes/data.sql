a772b7797b0012bed3db1aaeb2c3763a2155a427
Dani <dgarcia19@jaumebalmes.net>
2021-05-31 03:12:34 +0200

    push spring

---
+++ AndromedaProject/target/classes/data.sql
@@ -0,0 +1,180 @@
+INSERT INTO `cancion` (`id_cancion`, `cancion`, `genero`, `url`) VALUES
+(1, 'Feiticeira', 'Rock', '/media/Feiticeira - Deftones.mp3'),
+(2, 'Digital Bath', 'Rock', '/media/Digital Bath - Deftones.mp3'),
+(3, 'Elite', 'Rock', '/media/Elite - Deftones.mp3'),
+(4, 'RX Queen', 'Rock', '/media/RXQUEEN - Deftones.mp3'),
+(5, 'Street Carp', 'Rock', '/media/Street Carp - Deftones.mp3'),
+(6, 'Teenager', 'Rock', '/media/Teenager - Deftones'),
+(7, 'Knife Party', 'Rock', '/media/KnifeParty - Deftones.mp3'),
+(8, 'Korea', 'Rock', '/media/Korea - Deftones.mp3'),
+(9, 'Passenger', 'Rock', '/media/Passenger - Deftones.mp3'),
+(10, 'Change', 'Rock', '/media/Change - Deftones.mp3'),
+(11, 'Pink Maggit', 'Rock', '/media/PinkMaggit - Deftones.mp3'),
+(12, 'The Boys Republic', 'Rock', '/media/TheBoysRepublic - Deftones.mp3'),
+(13, 'Beware', 'Rock', '/media/Beware - Deftones.mp3'),
+(14, 'Cherry Waves', 'Rock', '/media/CherryWaves - Deftones.mp3'),
+(15, 'Combat', 'Rock', '/media/Combat - Deftones.mp3'),
+(16, 'HoleInTheEarth', 'Rock', '/media/HoleInTheEarth - Deftones.mp3'),
+(17, 'Kimdracula', 'Rock', '/media/Kimdracula - Deftones.mp3'),
+(18, 'Mein', 'Rock', '/media/Mein - Deftones.mp3'),
+(19, 'Pink Cellphone', 'Rock', '/media/PinkCellphone - Deftones.mp3'),
+(20, 'Rarture', 'Rock', '/media/Rarture - Deftones.mp3'),
+(21, 'RatsRatsRats', 'Rock', '/media/RatsRatsRats - Deftones.mp3'),
+(22, 'Riviere', 'Rock', '/media/Riviere - Deftones.mp3'),
+(23, 'Select Start', 'Rock', '/media/SelectStart - Deftones.mp3'),
+(24, 'Xerces', 'Rock', '/media/Xerces - Deftones.mp3'),
+(25, 'Heebie Jeebies', 'Jazz', '/media/Heebie Jeebies - Louis Armstrong.mp3'),
+(26, 'Alligator Crawl', 'Jazz', '/media/Alligator Crawl - Louis Armstrong.mp3'),
+(27, 'Big Butter and Egg', 'Jazz', '/media/Big Butter And Egg Man - Louis Armstrong.mp3'),
+(28, 'Cornet Chop Suey', 'Jazz', '/media/Cornet Chop Suey - Louis Armstrong.mp3'),
+(29, 'Jazz Lips', 'Jazz', '/media/Jazz Lips - Louis Armstrong.mp3'),
+(30, 'Melancholy Blues', 'Jazz', '/media/Melancholy Blues - Louis Armstrong.mp3'),
+(31, 'Muskrat Ramble', 'Jazz', '/media/Muskrat Ramble - Louis Armstrong.mp3'),
+(32, 'Potato Head Blues', 'Jazz', '/media/Potato Head Blues - Louis Armstrong.mp3'),
+(33, 'SKid-Dat-De-Dat', 'Jazz', '/media/SKid-Dat-De-Dat - Louis Armstrong.mp3'),
+(34, 'Weary Blues', 'Jazz', '/media/Weary Blues - Louis Armstrong.mp3'),
+(35, 'Wild Man Blues', 'Jazz', '/media/Wild Man Blues - Louis Armstrong.mp3'),
+(36, 'Willie The Weeper', 'Jazz', '/media/Willie The Weeper - Louis Armstrong.mp3'),
+(37, 'Intro', 'Hip-Hop', '/media/Intro - Eminem.mp3'),
+(38, 'Low Down, Dirty', 'Hip-Hop', '/media/Low Down Dirty - Eminem.mp3'),
+(39, 'If I Had...', 'Hip-Hop', '/media/If I Had - Eminem.mp3'),
+(40, 'Just Dont Give a Fuck', 'Hip-Hop', '/media/Just Dont Give a Fuck - Eminem.mp3'),
+(41, 'Mommy', 'Hip-Hop', '/media/Mommy - Eminem.mp3'),
+(42, 'Just the two of us', 'Hip-Hop', '/media/Just the two of us - Eminem.mp3'),
+(43, 'No ones Iller', 'Hip-Hop', '/media/No ones Iller - Eminem.mp3'),
+(44, 'Murder, Murder', 'Hip-Hop', '/media/Murder Murder - Eminem.mp3'),
+(45, '8 Misisipi', 'Hip-Hop', '/media/8 Misisipi - BEJO.mp3'),
+(46, 'El Ventilador', 'Hip-Hop', '/media/El Ventilador - BEJO.mp3'),
+(47, 'Metamorfosis', 'Hip-Hop', '/media/Metamorfosis - BEJO.mp3'),
+(48, 'Skit Loba', 'Hip-Hop', '/media/Skit Loba - BEJO.mp3'),
+(49, 'Vete ya de aquí', 'Hip-Hop', '/media/Vete ya de aquí - BEJO.mp3'),
+(50, 'La Prisión', 'Pop', '/media/La Prisión - Rels B.mp3'),
+(51, 'Yo tengo un ángel', 'Pop', '/media/Yo Tengo Un Ángel - Rels B.mp3'),
+(52, 'Una cumbia triste', 'Pop', '/media/Una Cumbia Triste - Rels B.mp3'),
+(53, 'Un verano en mallorca', 'Pop', '/media/Un Verano En Mallorca - Rels B.mp3'),
+(54, 'Me olvidé de los 2', 'Pop', '/media/Me Olvidé De Los 2 - Rels B.mp3'),
+(55, 'No te imaginas', 'Pop', '/media/No Te Imaginas - Rels B.mp3'),
+(56, 'Lágrimas de cocodrilo', 'Pop', '/media/Lágrimas De Cocodrilo - Rels B.mp3'),
+(57, 'Libre sin dinero', 'Pop', '/media/Libre Sin Dinero - Rels B.mp3'),
+(58, 'Culpable', 'Pop', '/media/Culpable - Rels B.mp3'),
+(59, 'La última canción', 'Pop', '/media/La Última Canción - Rels B.mp3'),
+(60, 'Would', 'Grunge', '/media/Would - Alice in Chains.mp3'),
+(61, 'No buses', 'Rock', '/media/No buses - Arctic Monkeys.mp3'),
+(62, 'Snap out of it', 'Rock', '/media/Snap out of it - Arctic Monkeys.mp3'),
+(63, 'Positif siempre', 'Pop', '/media/Positif siempre - Chico Blanco.mp3'),
+(64, 'Islandia Freestyle', 'Hip-Hop', '/media/Islanda Freestyle - Dutch Nazari.mp3'),
+(65, 'Cocaine', 'Blues', '/media/Cocaine - Eric Clapton.mp3'),
+(66, 'Magnetic', 'Neurofunk', '/media/Magnetic - Future Prophecies.mp3'),
+(67, 'Entre dos tierras', 'Rock Nacional', '/media/Entre dos tierras - Heroes del Silencio.mp3'),
+(68, 'Kiss kiss bang bang', 'Drum and Bass', '/media/kiss_kiss_bang_bang - High Contrast.mp3'),
+(69, 'Brodo', 'Hip-Hop', '/media/brodo - laila al habash.mp3'),
+(70, 'Andaluz de nacimiento', 'Rock nacional', '/media/Andaluz de nacimiento - Hora Zulu.mp3'),
+(71, 'Lost on you', 'Indie', '/media/Lost on you - LP.mp3'),
+(72, 'Slytherin', 'Trance', '/media/Slytherin - Mandragora.mp3'),
+(73, 'La maldición', 'Punk', '/media/La maldición - Non Servium.mp3'),
+(74, 'Talismán', 'Hip-Hop', '/media/Talismán - Sharif.mp3'),
+(75, 'Levitate', 'Drum and Bass', '/media/Levitate - Spor.mp3'),
+(76, 'Every breath you take', 'Rock', '/media/Every_Breath_You_Take - The Police.mp3'),
+(77, 'Uplifting chorus', 'Post Rock', '/media/Uplifting - Yvette.mp3');
+
+INSERT INTO `artista` (`id_artista`, `artista`) VALUES
+(1, 'Deftones'),
+(2,'Louis Armstrong'),
+(3,'Eminem'),
+(4,'Bejo'),
+(5,'Rels B'),
+(6,'Alice in Chains'),
+(7,'Arctic Monkeys'),
+(8,'Chico Blanco'),
+(9,'Dutch Nazari'),
+(10,'Eric Clapton'),
+(11,'Future Prophecies'),
+(12,'Heroes del Silencio'),
+(13,'High Contrast'),
+(14,'Hora Zulu'),
+(15,'Laila al Habash'),
+(16,'LP'),
+(17,'Mandragora'),
+(18,'Non Servium'),
+(19,'Sharif'),
+(20,'Spor'),
+(21,'The Police'),
+(22,'Yvette');
+
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (1, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (2, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (3, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (4, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (5, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (6, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (7, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (8, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (9, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (10, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (11, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (12, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (13, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (14, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (15, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (16, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (17, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (18, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (19, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (20, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (21, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (22, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (23, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (24, 1);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (25, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (26, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (27, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (28, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (29, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (30, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (31, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (32, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (33, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (34, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (35, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (36, 2);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (37, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (38, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (39, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (40, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (41, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (42, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (43, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (44, 3);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (45, 4);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (46, 4);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (47, 4);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (48, 4);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (49, 4);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (50, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (51, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (52, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (53, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (54, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (55, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (56, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (57, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (58, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (59, 5);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (60, 6);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (61, 7);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (62, 7);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (63, 8);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (64, 9);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (65, 10);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (66, 11);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (67, 12);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (68, 13);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (69, 15);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (70, 14);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (71, 16);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (72, 17);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (73, 18);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (74, 19);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (75, 20);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (76, 21);
+INSERT INTO `cancion_artista` (`id_cancion`, `id_artista`) VALUES (77, 22);
